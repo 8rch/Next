ad37050d4c2f02cb64e6802bed31e9988ffe2864
8rch <brchamorro@hotmail.com>
2023-07-16 22:47:29 -0500

    modified:   NextAth/next_app_login/src/app/components/Navbar.tsx 	modified:   NextAth/next_app_login/src/app/page.tsx

--- NextAth/next_app_login/src/app/components/Navbar.tsx
+++ NextAth/next_app_login/src/app/components/Navbar.tsx
@@ -5,7 +5,7 @@ export default function Navbar() {
 		<nav className='bg-blue-800 p-4'>
 			<ul className='flex justify-evenly text-2xl font-bold'>
 				<li>
-					<Link href='/'>Home</Link>
+					<Link href='/'>Inicio</Link>
 				</li>
 				<li>
 					<Link href='/api/auth/signin'>Sign In</Link>
--- NextAth/next_app_login/src/app/page.tsx
+++ NextAth/next_app_login/src/app/page.tsx
@@ -1,3 +1,3 @@
 export default async function Home() {
-	return <h1 className='text-5xl'>Pagina inicio </h1>;
+	return <h1 className='text-5xl'>Pagina de inicio </h1>;
 }
